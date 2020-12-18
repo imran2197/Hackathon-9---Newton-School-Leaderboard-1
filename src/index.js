cb805205604a4ac09ca707b53acaa7a66992c804
Manjunath Aithal <manjunathaithal19@gmail.com>
2020-12-19 02:27:29 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 8080
+const port = 8080;
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
-const { data } = require('./data')
+const data = require("./data");
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // your code goes here
 
+app.get("/topRankings", (req, res) => {
+  const limit = req.query.limit;
+  const offset = req.query.offset;
+  const limits = limit === undefined || isNaN(limit) ? 20 : Number(limit);
+  const offsets = offset === undefined || isNaN(offset) ? 0 : Number(offset);
+  console.log(offsets, limits);
+  const value = data.data.slice(offsets, limits + offsets);
+  res.send(value);
+});
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
